46d117b6c87c227435c44b9238d71875f12f6d4e
Glitch (kc-at-panel) <none>
2019-10-25 18:40:41 +0000

    🍦⛄️ Checkpoint ./config.js:5880116/181 ./ayarlar.json:5880116/108 ./events/ready.js:5880116/72

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-  "token": "",
+  "token": "NjM3MzU2NTEwMDY5OTE1NjUw.XbNAng.4O0PEh4kUV7o1PeA5xCHcwH9P28",
 
   "dashboard" : {
-    "oauthSecret": "", // botunuzun client secret tini buraya yazin
-    "callbackURL": "https://kc-at-panel.glitch.me/callback",// burda login yapmak icin botunuzun Outh2 yazan yere gelip orda daki kizma yapistirip kaydedin
-    "sessionSecret": "",
-    "domain": "https://kc-at-panel.glitch.me",// buraya kc at alt yapi yerine yazdiysaniz kc-at-panel isimi ile degistirin
+    "oauthSecret": "uk2R5brX0joQnsbgswXZyVOJXR62swG7", // botunuzun client secret tini buraya yazin
+    "callbackURL": "https://snax-kontrol-panel.glitch.me/callback",// burda login yapmak icin botunuzun Outh2 yazan yere gelip orda daki kizma yapistirip kaydedin
+    "sessionSecret": "uk2R5brX0joQnsbgswXZyVOJXR62swG7",
+    "domain": "https://snax-kontrol-panel.glitch.me",// buraya kc at alt yapi yerine yazdiysaniz kc-at-panel isimi ile degistirin
     "port": 8000
   },
 };
