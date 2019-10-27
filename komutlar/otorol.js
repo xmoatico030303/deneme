6aaa2a38e2285b4f3a62f4b08ab32886ffee0af9
Glitch (kc-at-panel) <none>
2019-10-27 11:19:49 +0000

    🛤⛸ Checkpoint ./komutlar/otorol.js:5880116/3226

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -1,44 +1,33 @@
-const Discord = require('discord.js')
-const db = require('quick.db');
-const ayarlar = require('../ayarlar.json')
-exports.run = async (client, message, args) => {
-  
-var prefix = ayarlar.prefix;
-  
-if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.channel.send(`Bu işlemi gerçekleştirmem için "\`Rolleri Yönet\`" yetkisine sahip olmalıyım.`)   
-  if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`Bu komutu kullanabilmek için "\`Sunucuyu Yönet\`" yetkisine sahip olmalısın.`);
-  let mesaj = args.slice(0).join(' ')
-  let otorol= db.fetch(`otorol_${message.guild.id}`)
-  let rol = message.mentions.roles.first()
-  let rolk = message.mentions.channels.first() 
-  if(mesaj === "kapat") {
-    if(!otorol) {
-      message.channel.send(`Bu sunucuda otorol ayarlanmamış.`)
-      return
-    } 
-    db.delete(`otorol_${message.guild.id}`)
-    db.delete(`otolog_${message.guild.id}`)
-    message.channel.send(`Otorol başarıyla kapatıldı.`)
-    return
-  }
-  if (!rol) return message.channel.send(`Giriş yapanlara vereceğim rolü belirtmelisin. Örnek: \`${prefix}otorol @Rol #Kanal\``)
-  if (!rolk) return message.channel.send(`Rol verildiğinde bildirimin gideceği kanalı belirtmelisin. Örnek: \`${prefix}otorol @Rol #Kanal\``)
-  
-  db.set(`otorol_${message.guild.id}`, rol.id)
-  db.set(`otolog_${message.guild.id}` ,rolk.id)
-  
-  message.channel.send(`Giriş yapanlara verilecek rol \`${rol.name}\`, bildirimin gideceği kanal ise ${rolk} olarak ayarlandı.`)
-   };
-    
+const Discord = require('discord.js');
+const loglar = require('../ayarlar.json');
+
+var prefix = loglar.prefix;
+
+exports.run = async (client, message, params, args) => {
+
+  const eğlence = new Discord.RichEmbed()
+  .setColor(0x36393E)
+      .setAuthor(`Snax Bot`, client.user.avatarURL)
+      .setThumbnail(client.user.avatarURL)
+.addField("<a:f1:637967475530530816>| Otorol Ayarlamak İçin s!otorol @rol #kanal ")
+.addField("<a:f1:637967475530530816>| Gold Üye |<a:f1:637967475530530816> ", "Gold Üye Size Ne Gibi Katkılar Sağlar \n Otorol Mesaj Degiştire Bilirsin")
+.addField("<a:f1:637967475530530816>| !oto-rol-msg 》 Otorol Mesajını Ayarlar Gold Üye İçindir. |<a:f1:637967475530530816> ", "Örnek: `s!otorolmsg -server-, Sunucumuza Hoşgeldin, -uye-! -rol- Adlı Rolün Başarı İle Verildi Seninle Beraber, **-uyesayisi-** Kişiyiz.``")
+
+          
+
+return message.channel.sendEmbed(eğlence);
+
+};
+
 exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliases: ['girişrol'],
-    permLevel: 2
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
 };
+  
 exports.help = {
-    name: 'otorol',
-    description: 'Sunucuya giren kullanıcıya seçtiğiniz rolü otomatik verir.',
-    usage: 'otorol <@rol> <#kanal>'
+  name: 'otorol',
+  description: 'Komut kategorilerini gösterir.',
+  usage: 'eğlence'
 };
-//XiR
