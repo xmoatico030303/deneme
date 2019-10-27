c300b7c29f88f2588bda9b16c6e9d0d07e5e92ed
Glitch (kc-at-panel) <none>
2019-10-27 11:39:48 +0000

    👾😇 Checkpoint ./komutlar/oto-rol-ayarla.js:5880116/43 ./komutlar/otorol.js:5880116/40 ./komutlar/gold-üye.js:5880116/1574 ./bot.js:5880116/30759

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -9,9 +9,9 @@ exports.run = async (client, message, params, args) => {
   .setColor(0x36393E)
       .setAuthor(`Snax Bot`, client.user.avatarURL)
       .setThumbnail(client.user.avatarURL)
-.addField("<a:f1:637967475530530816>| Otorol Ayarlamak İçin s!otorol @rol #kanal ", "Bu Kod Normal Kullanıcılar İçindir")
+.addField("<a:f1:637967475530530816>| Otorol Ayarlamak İçin s!oto-rol-ayarla @rol #kanal ", "Bu Kod Normal Kullanıcılar İçindir")
 .addField("<a:f1:637967475530530816>| Gold Üye |<a:f1:637967475530530816> ", "Gold Üye Size Ne Gibi Katkılar Sağlar \n Otorol Mesaj Degiştire Bilirsin")
-.addField("<a:f1:637967475530530816>| !oto-rol-msg 》 Otorol Mesajını Ayarlar Gold Üye İçindir. |<a:f1:637967475530530816> ", "Örnek: `s!otorolmsg -server-, Sunucumuza Hoşgeldin, -uye-! -rol- Adlı Rolün Başarı İle Verildi Seninle Beraber, **-uyesayisi-** Kişiyiz.`")
+.addField("<a:f1:637967475530530816>| !oto-rol-msg 》 Otorol Mesajını Ayarlar Gold Üye İçindir. |<a:f1:637967475530530816> ", "Örnek: `s!oto-rol-msg -server-, Sunucumuza Hoşgeldin, -uye-! -rol- Adlı Rolün Başarı İle Verildi Seninle Beraber, **-uyesayisi-** Kişiyiz.`")
 .addField("<a:f1:637967475530530816>| Gold Üye |<a:f1:637967475530530816> ", "Gold Üyeler Otorol Mesaj Ekleye Bilece Kod")
 .addField("<a:f1:637967475530530816>| Gold Üye |<a:f1:637967475530530816> ", "**-uye- Katılan Kullanıcı İsmi \n -rol- Verilecek Rol İsmi \n -server- Sunucu İsmi \n -uyesayisi- Sunucudaki Üye Sayısı Toplam \n -botsayisi- Sunucudaki Toplam Bot Sayısı \n -bolge- Sunucun Bolgesi \n -kanalsayisi- Sunucudaki Toplam Kanal Sayısı**")
           
